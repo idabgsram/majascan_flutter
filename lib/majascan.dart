adfcbcdc81de0086fe31579dd372d01ec62bb8fa
Gusram <idabgsram@gmail.com>
2024-07-01 18:51:41 +0800

    update dependencies

--- lib/majascan.dart
+++ lib/majascan.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-import 'dart:ffi';
-import 'dart:ui';
 import 'package:flutter/services.dart';
 
-import 'package:flutter/widgets.dart';
 
 class MajaScan {
   static const MethodChannel _channel = const MethodChannel('majascan');
--- test/majascan_test.dart
+++ test/majascan_test.dart
@@ -1,18 +1,16 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:majascan/majascan.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('majascan');
+  // const MethodChannel channel = MethodChannel('majascan');
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    // channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    //   return '42';
+    // });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    // channel.setMockMethodCallHandler(null);
   });
 
 //  test('getPlatformVersion', () async {
